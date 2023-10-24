997ac27b960a6b3fe2f4ddafc91078d212199c8b
Bonbon <mail@jan-schmitt.eu>
2023-10-24 14:43:56 +0200

    created power save mode

--- main.lua
+++ main.lua
@@ -4,20 +4,27 @@ function love.load()
 end
 
 function love.update()
-    if love.keyboard.isDown("s") then
-        y = y + 2
-    end
-    if love.keyboard.isDown("w") then
-        y = y - 2
-    end
-    if love.keyboard.isDown("a") then
-        x = x - 2
-    end
-    if love.keyboard.isDown("d") then
-        x = x + 2
+    if love.window.isVisible() then
+        if love.keyboard.isDown("s") then
+            y = y + 2
+        end
+        if love.keyboard.isDown("w") then
+            y = y - 2
+        end
+        if love.keyboard.isDown("a") then
+            x = x - 2
+        end
+        if love.keyboard.isDown("d") then
+            x = x + 2
+        end
     end
 end
 
 function love.draw()
+    red = 0/255
+    green = 153/255
+    blue = 204/255
+    alpha = 100/100
+    love.graphics.setBackgroundColor( red, green, blue, alpha)
     love.graphics.print("Hello World", x, y)
 end
