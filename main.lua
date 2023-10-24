8cdb12e2189dd91a33a42fb76672f789b1034b65
Bonbon <mail@jan-schmitt.eu>
2023-10-24 15:17:53 +0200

    added gravity

--- main.lua
+++ main.lua
@@ -1,27 +1,37 @@
 function love.load()
     x = 400
     y = 300
-
+    isloggedcycle = 0
+    speed = 2
 end
 
 function love.update()
     if love.window.isVisible() then
         if love.keyboard.isDown("s") then
-            y = y + 2
+            y = y + speed
         end
         if love.keyboard.isDown("w") then
-            y = y - 2
+            y = y - speed
+        end
+        if love.keyboard.isDown("up") then
+            isloggedcycle = 20
         end
         if love.keyboard.isDown("a") then
-            x = x - 2
+            x = x - speed
         end
         if love.keyboard.isDown("d") then
-            x = x + 2
+            x = x + speed
         end
-        if love.keyboard.isDown("b") then
+        if love.keyboard.isDown("escape") then
             love.window.close()
             love.event.quit()
         end
+        if isloggedcycle >= 0 then
+            y = y - 10
+            isloggedcycle = isloggedcycle - 1
+        end
+        y = y + 5
+        speed = speed + 0.002
     end
 end
 
