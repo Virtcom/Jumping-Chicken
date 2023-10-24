f84ba9c0231f517956202604fcfa04f8cc233d23
Bonbon <mail@jan-schmitt.eu>
2023-10-24 14:35:30 +0200

    made moving logo

--- main.lua
+++ main.lua
@@ -1,4 +1,23 @@
+function love.load()
+    x = 400
+    y = 300
+end
+
+function love.update()
+    if love.keyboard.isDown("s") then
+        y = y + 2
+    end
+    if love.keyboard.isDown("w") then
+        y = y - 2
+    end
+    if love.keyboard.isDown("a") then
+        x = x - 2
+    end
+    if love.keyboard.isDown("d") then
+        x = x + 2
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print("Hello World", x, y)
 end
